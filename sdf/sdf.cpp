3ad92c467504023e5c1f352515fe5bf0e9be29a4
AkimenkoAV <AkimenkoAV@C7A104-5R>
2025-04-15 19:14:54 +0300

    Добавьте файлы проекта.

---
+++ sdf/sdf.cpp
@@ -0,0 +1,10 @@
+﻿// sdf.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+} 
+asdas
